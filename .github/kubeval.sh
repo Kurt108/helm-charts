58be48fc758b5943cb7c5d58c905fbf03bd43969
Kurt <10924313+Kurt108@users.noreply.github.com>
2024-07-15 11:08:46 +0000

    fix(ci): bump more deps

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 CHART_DIRS="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/master -- charts | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yamlg')"
-KUBEVAL_VERSION="0.14.0"
+KUBEVAL_VERSION="0.16.1"
 SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/"
 
 # install kubeval
